c798ba8bd40f42e995b920d07106f42b677ea351
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-02-11 00:00:08 +0100

    new component
    search button icon component

--- refund/src/App.tsx
+++ refund/src/App.tsx
@@ -1,9 +1,11 @@
 import Button from "./components/button";
+import { ButtonSearch } from "./components/buttonSearch";
 
 function App() {
   return (
-    <div className="">
+    <div className="flex flex-col gap-2 p-1">
       <Button size="md">Send</Button>
+      <ButtonSearch size="md" />
     </div>
   );
 }
