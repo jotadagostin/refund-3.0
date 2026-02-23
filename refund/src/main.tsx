2bdbddd60dc43b3c2761528c8556146107efa86f
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-02-23 20:09:39 +0100

    Update main.tsx

--- refund/src/main.tsx
+++ refund/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import React, { Suspense } from "react";
 import { RefundProvider } from "./context/refundProvider.tsx";
 import { LanguageProvider } from "./context/languageProvider.tsx";
@@ -11,11 +11,11 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Suspense fallback={null}>
       <LanguageProvider>
-        <BrowserRouter>
+        <HashRouter>
           <RefundProvider>
             <App />
           </RefundProvider>
-        </BrowserRouter>
+        </HashRouter>
       </LanguageProvider>
     </Suspense>
   </React.StrictMode>,
